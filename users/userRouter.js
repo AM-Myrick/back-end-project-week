f23072292a2070b2ea6ba7993a1005352ad670a1
AM-Myrick <amyrick.820@gmail.com>
2020-03-06 22:20:40 -0500

    finish change-password route

--- users/userRouter.js
+++ users/userRouter.js
@@ -4,6 +4,8 @@ const knexConfig = require("../knexfile");
 const db = process.env.NODE_ENV ? knex(knexConfig.production) : knex(knexConfig.development);
 const router = express.Router();
 const { authenticate } = require('../middleware.js');
+const bcrypt = require("bcryptjs");
+const saltRounds = +process.env.hash || require("../_secrets/keys.js").hash;
 
 router.get("/", (req, res) => {
     db("users")
@@ -12,19 +14,18 @@ router.get("/", (req, res) => {
 })
 
 router.patch("/change-password", authenticate, (req, res) => {
-    const { username } = req.decoded;
-    const { password } = req.body;
+    let { username, password } = req.body.creds;
 
     if (password === null) {
         return res.status(405).json({error: "Please enter a password."});
     }
 
+    password = bcrypt.hashSync(password, saltRounds);
     db("users")
         .where({ username })
-        .first()
         .update({ password })
         .then(count => res.status(200).json(count))
-        .catch(error => res.status(500).json({error}))
+        .catch(error => res.status(500).json({ error }))
 })
 
 module.exports = router;

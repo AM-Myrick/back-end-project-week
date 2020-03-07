22046db9f48e1bb88b7527ef1f6438189c7ff426
AM-Myrick <amyrick.820@gmail.com>
2020-03-06 22:50:12 -0500

    middleware refactor

--- middleware.js
+++ middleware.js
@@ -1,32 +1,33 @@
-const jwt = require('jsonwebtoken');
-const jwtKey = process.env.jwtKey || require('./_secrets/keys').jwtKey;
+const jwt = require("jsonwebtoken");
+const jwtKey = process.env.jwtKey || require("./_secrets/keys").jwtKey;
 
-function authenticate(req, res, next) {
-  const token = req.get('Authorization');
+const authenticate = (req, res, next) => {
+  const token = req.get("Authorization");
 
   if (token) {
     jwt.verify(token, jwtKey, (err, decoded) => {
-      if (err) return res.status(401).json(err);
+      if (err) {
+        return res.status(401).json(err);
+      }
 
       req.decoded = decoded;
       next();
     });
-  } else {
-    return res.status(401).json({
-      error: 'No token provided, must be set on the Authorization Header',
-    });
   }
-}
+  return res.status(401).json({
+    error: "No token provided, must be set on the Authorization Header"
+  });
+};
 
 function generateToken(user) {
   const payload = {
     subject: user.id,
-    username: user.username,
+    username: user.username
   };
 
   const secret = jwtKey;
   const options = {
-    expiresIn: '1w',
+    expiresIn: "1w"
   };
 
   return jwt.sign(payload, secret, options);
